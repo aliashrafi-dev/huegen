cf64dd675db1b1988e9c623d83afb47e8ded8807
AliAshrafi99 <Ali.Ashrafi99@yahoo.com>
2026-08-30 16:28:40 +0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,52 @@
-import Image from "next/image";
+import { Particles } from "@/components/ui/particles"
+import {
+  ScrollVelocityContainer,
+  ScrollVelocityRow,
+} from "@/components/ui/scroll-based-velocity"
+import Image from "next/image"
+import { TextAnimate } from "@/components/ui/text-animate"
+
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <main className="relative min-h-screen w-full">
+    
+      <div className="fixed inset-0 -z-10 h-screen w-screen pointer-events-none overflow-hidden">
+        <Particles quantity={100} ease={80} refresh={false} />
+      </div>
+
+      <div className="flex flex-col items-center justify-center text-6xl md:text-5xl font-extrabold translate-y-150">
+          <TextAnimate animation="blurInUp" by="character" duration={5}>
+      Color made simple
+    </TextAnimate>
     </div>
-  );
+
+      
+      <div className="flex min-h-screen items-center justify-center py-20 translate-y-40">
+        <ScrollVelocityContainer className="text-4xl font-bold md:text-7xl">
+          <ScrollVelocityRow baseVelocity={20} direction={1}>
+            
+            <div className="flex items-center gap-6 px-3">
+              <img src="/photo1.jpg" alt="1" className="h-40 w-60 rounded-xl object-cover" />
+              <img src="/photo2.jpg" alt="2" className="h-40 w-60 rounded-xl object-cover" />
+              <img src="/photo3.jpg" alt="3" className="h-40 w-60 rounded-xl object-cover" />
+              <img src="/photo4.jpg" alt="3" className="h-40 w-60 rounded-xl object-cover" />
+              <img src="/photo5.jpg" alt="3" className="h-40 w-60 rounded-xl object-cover" />
+            </div>
+          </ScrollVelocityRow>
+
+          <ScrollVelocityRow baseVelocity={20} direction={-1}>
+                        <div className="flex items-center gap-6 px-3">
+              <img src="/img1.jpeg" alt="1" className="h-40 w-60 rounded-xl object-cover" />
+              <img src="/img2.jpeg" alt="2" className="h-40 w-60 rounded-xl object-cover" />
+              <img src="/image3.jpeg" alt="3" className="h-40 w-60 rounded-xl object-cover" />
+              <img src="/image4.jpeg" alt="3" className="h-40 w-60 rounded-xl object-cover" />
+              <img src="/image5.jpeg" alt="3" className="h-40 w-60 rounded-xl object-cover" />
+            </div>
+          </ScrollVelocityRow>
+        </ScrollVelocityContainer>
+      </div>
+    </main>
+  )
 }
